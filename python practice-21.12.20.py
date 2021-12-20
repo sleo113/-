d4c1ff4d1bcbbf2dd5ea56bb90977bc7d5645d69
JeUk Park <77144006+sleo113@users.noreply.github.com>
2021-12-20 17:44:54 +0900

    Add files via upload

---
+++ python practice-21.12.20.py
@@ -0,0 +1,30 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # 변수 표현해보기.
+>>> x=1
+>>> y=2
+>>> z=x+y
+>>> print(z)
+3
+>>> x="대한"
+>>> y="민국"
+>>> z=x+y
+>>> print(z)
+대한민국
+>>> x='i'
+>>> y='ain t'
+>>> z='바보'
+>>> u=x+y+z
+>>> print(u)
+iain t바보
+>>> x=10
+>>> y=2
+>>> z=8
+>>> t=9
+>>> print(z**t)
+134217728
+>>> print(x+y*(y/x))
+10.4
+>>> print(t%x)
+9
+>>> 
