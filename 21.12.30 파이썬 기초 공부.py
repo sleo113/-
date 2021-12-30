b097a7caf25780c3a316287e05c1a84982b44913
JeUk Park <77144006+sleo113@users.noreply.github.com>
2021-12-30 20:19:17 +0900

    Add files via upload

---
+++ 21.12.30 파이썬 기초 공부.py
@@ -0,0 +1,21 @@
+# 1. station 변수에 이름 3가지 정의하기
+# 2. 전철이 들어오는 멘트 사용하여 출력하기.
+
+station = "사당"
+print(station+"행 열차가 들어오고 있습니다.\n")
+
+station = "신도림"
+print(station+"행 열차가 들어오고 있습니다.\n")
+
+station = "인천공항"
+print(station + "행 열차가 들어오고 있습니다.")
+
+station1 = "사당"
+station2 = "신도림"
+station3 = "인천공항"
+
+print("\n" + station1 + "행 열차가 들어오고 있습니다.\n ")
+
+print("\n" + station2 + "행 열차가 들어오고 있습니다.\n ")
+
+print("\n" + station3 + "행 열차가 들어오고 있습니다.\n ")
